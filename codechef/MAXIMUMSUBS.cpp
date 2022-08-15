053a6571fecbd4abb5b4dd10354036384f304cb1
crypt3d-c00kie <cookie.crypted@gmail.com>
2022-08-16 01:13:34 +0530

    Add files via upload

---
+++ codechef/MAXIMUMSUBS.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int nSize;
+    cin >> nSize;
+	int nArr[10];
+	
+	for(int i=0;i<nSize;i++)
+	{
+	    cin >> nArr[i];
+	    nArr[i] = nArr[i]*2;
+	}
+	for(int i=0;i<nSize;i++)
+	    cout << nArr[i] << endl;
+	return 0;
+}
