07bb4a5786481beb3ae4eaf5abfa03c8a3abe2fe
crypt3d-c00kie <cookie.crypted@gmail.com>
2022-08-18 13:05:59 +0530

    Add files via upload

---
+++ leetcode/121-stock-buy-and-sell.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// maximize the profit
+// if the prices are in decreasing order then profit = 0 no matter what
+
+int maxProfit(vector<int>& prices) {
+    int maxProfit = 0;
+    int minProfit = INT_MAX;
+        
+    for(int i=0;i<prices.size();i++){
+        if(prices[i] < minProfit)
+            minProfit = prices[i];
+        else if(prices[i] - minProfit > maxProfit)
+            maxProfit = prices[i] - minProfit; 
+    }
+        
+    return maxProfit;
+} 
+int main(){
+    vector<int> given;
+    int profit;
+    int nSize;
+    cout << "Enter number of elements :: ";
+    cin >> nSize;
+    
+    cout << "Stock prices :: ";
+    for(int i=0;i<nSize;i++){
+        int tmp;
+        cin >> tmp;
+        given.push_back(tmp);
+
+    }
+    profit = maxProfit(given);
+    cout << "maxProfit :: " << profit << endl;
+    return 0;
+}
