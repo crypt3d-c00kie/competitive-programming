3884a056bd52a17587b31bde172a20d4e86bbe0e
crypt3d-c00kie <cookie.crypted@gmail.com>
2022-09-08 10:03:24 +0530

    Add files via upload

---
+++ codechef/FEVER.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int x;
+        cin >> x;
+        if(x > 98)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+    return 0;
+}
