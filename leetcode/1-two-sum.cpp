073f19caa926240272148b3043da5d91b2192455
crypt3d-c00kie <cookie.crypted@gmail.com>
2022-10-14 22:57:51 +0530

    Update 1-two-sum.cpp

--- leetcode/1-two-sum.cpp
+++ leetcode/1-two-sum.cpp
@@ -6,8 +6,8 @@ vector<int> twoSum(vector<int>& nums, int target) {
     for(int i=0;i<nums.size();i++){
         int temp = target - nums[i];
         if(m.find(temp) != m.end()){
-        resultant.push_back(m[temp]);
-        resultant.push_back(i);
+            resultant.push_back(m[temp]);
+            resultant.push_back(i);
         }
     
         m[nums[i]] = i;
