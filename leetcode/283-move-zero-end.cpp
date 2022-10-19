60f84701fb41dfb9b7759664669204174fde8d4f
crypt3d-c00kie <cookie.crypted@gmail.com>
2022-10-19 20:09:47 +0530

    Add files via upload

---
+++ leetcode/283-move-zero-end.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void moveZeroes(vector<int>& nums) {
+    int snowball = 0;
+    for(int i=0;i<nums.size();i++){
+        if(nums[i] == 0)
+            snowball++;
+        else if(snowball > 0){
+            int tmp = nums[i];
+            nums[i] = 0;
+            nums[i-snowball] = tmp;
+        }
+    }
+}
+int main(){
+    int n;
+    vector<int> given;
+    cout << "Number of elements :: ";
+    cin >> n;
+    cout << "Given  :: ";
+    for(int i=0;i<n;i++){
+        int tmp;
+        cin >> tmp;
+        given.push_back(tmp);
+    }
+    
+    cout << "Resultant :: ";
+    moveZeroes(given);
+    for(auto x : given)
+        cout << x << " ";
+        
+    return 0;
+}
