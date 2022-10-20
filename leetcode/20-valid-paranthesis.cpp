80029655a5fc61675b91604bb361e0dc65d39791
crypt3d-c00kie <cookie.crypted@gmail.com>
2022-10-20 23:01:48 +0530

    Add files via upload

---
+++ leetcode/20-valid-paranthesis.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isValid(string given){
+    stack<char> st;
+    for(auto x : given){
+        if(x == '(' or x == '[' or x == '{'){
+            st.push(x);
+        }
+        else{
+            if(st.empty() or (st.top() == '(' and x != ')') or (st.top() == '[' and x != ']') or (st.top() == '{' and x != '}'))
+                return false;
+            st.pop();
+        }
+    }
+    return st.empty();
+}
+int main(){
+    string given;
+    cout << "Given :: ";
+    cin >> given;
+    cout << "Valid or not? :: " << isValid(given);
+    return 0;
+}
