7c25696416036c00b0a360a1d6f3ef356354d4b3
crypt3d-c00kie <cookie.crypted@gmail.com>
2023-03-20 23:41:19 +0530

    Update 122-best-time-to-buy-sell-stock-2.java

--- leetcode/122-best-time-to-buy-sell-stock-2.java
+++ leetcode/122-best-time-to-buy-sell-stock-2.java
@@ -1,7 +1,7 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int maxProfit = 0;
-        
+         
         for(int i=0;i<prices.length-1;i++){
             if(prices[i] < prices[i+1]){
                 maxProfit += (prices[i+1] - prices[i]);      
