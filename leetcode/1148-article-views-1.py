133f91671d0d96b37ed4fa0b0af45ad4b52afd12
crypt3d-c00kie <cookie.crypted@gmail.com>
2023-10-13 09:23:57 +0530

    Add files via upload

---
+++ leetcode/1148-article-views-1.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def article_views(views: pd.DataFrame) -> pd.DataFrame:
+    authorsWithOwn = views[views['viewer_id'] == views['author_id']][['viewer_id']].drop_duplicates()
+    authorsWithOwn = authorsWithOwn.rename(columns={'viewer_id': 'id'})
+    authorsWithOwn = authorsWithOwn.sort_values('id')
+    
+    return authorsWithOwn
